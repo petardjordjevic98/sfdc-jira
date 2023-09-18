e7181cd3d1b3ba9b6dcf407828b36f8716150e12
Petar Djordjevic <71548362+petardjordjevic98@users.noreply.github.com>
2023-09-18 11:56:39 +0200

    s

--- parsePRfiles.js
+++ parsePRfiles.js
@@ -5,20 +5,20 @@ async function extractTests() {
 
     //by default we specify that all tests should run
     let testsFile = __dirname + '/compsToRun.txt';
-    await fs.promises.writeFile(testsFile, 'all');
+    fs.promises.writeFile(testsFile, 'all');
 
     const lines = readline.createInterface({
         input: fs.createReadStream(__dirname + '/components.txt'),
         crlfDelay: Infinity
     });
 
-    for await (const line of lines) {
+    for (const line of lines) {
         //special delimeter for apex tests
         let f = line.split('###')
-        for await (let f1 of f) {
+        for (let f1 of f) {
             console.log(f1)
-            await fs.promises.writeFile(testsFile, f1);
-            await fs.promises.appendFile(testsFile, '\n');
+            fs.promises.writeFile(testsFile, f1);
+            fs.promises.appendFile(testsFile, '\n');
         }
 
         //remove after dot 
